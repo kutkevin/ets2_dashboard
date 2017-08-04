69dbfef4a2cd20fccdd5bdf3ae72d1188a8350e1
kutkevin <30722848+kutkevin@users.noreply.github.com>
2017-08-04 12:33:12 +0200

    Update build.bat
    for my pc

--- build/build.bat
+++ build/build.bat
@@ -1,7 +1,7 @@
 @echo off
 
 set SDK_INC_DIR=C:\Users\Silas\Downloads\2013\scs_sdk_1_0\include
-set INSTALL_DIR=C:\Program Files (x86)\Steam\steamapps\common\Euro Truck Simulator 2\bin\win_x86\plugins
+set INSTALL_DIR=F:\Program Files (x86)\Steam\steamapps\common\Euro Truck Simulator 2\bin\win_x86\plugins
 set SRC_DIR=..\source\ets2_plugin
 
 set SRC_FILES=%SRC_DIR%\plugin.def %SRC_DIR%\plugin.cpp %SRC_DIR%\serial.cpp %SRC_DIR%\options.cpp
